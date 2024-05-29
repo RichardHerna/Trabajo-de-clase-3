16a0296a429ccd22026c4d0f1695f43a244bbc7c
RichardHerna <20220194@ricaldone.edu.sv>
2024-05-29 14:48:14 -0600

    Subiendo ejercicio

---
+++ src/data/dataEx.js
@@ -0,0 +1,19 @@
+const dataEX = [
+    {
+      id: '1',
+      title: '#00D410',
+      src: require('../img/img1.jpg'),
+    },
+    {
+      id: '2',
+      title: '#ECAC90',
+      src: require('../img/img2.jpg'),
+    },
+    {
+      id: '3',
+      title: '#00605A',
+      src: require('../img/img3.jpg'),
+    }
+  ];
+ 
+  export default dataEX;
